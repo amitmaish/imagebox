6812b4d6e63865f1a8d80d19fb06c73a27347f81
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-01-20 18:53:11 -0800

    added image reading and writing, including to the standard output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,129 @@
+use image;
+use std::env;
+use std::io::{self, Cursor, Read, Write};
+use std::path::Path;
+
+#[derive(Debug)]
+enum Input<'a> {
+    Pipe,
+    Path(Option<&'a Path>),
+}
+
+#[derive(Debug)]
+enum Output {
+    Dump,
+    Path(String),
+}
+
+#[derive(Debug)]
+struct Configuration<'a> {
+    input: Input<'a>,
+    output: Output,
+}
+
+#[derive(Debug)]
+enum Effects {
+    _Pass,
+    _Blur(f32),
+    _Invert,
+}
+
+#[derive(Debug)]
+struct Layer {
+    image: image::DynamicImage,
+    _effect_chain: Vec<Effects>,
+}
+
+impl Layer {
+    fn new(image: image::DynamicImage) -> Self {
+        Self {
+            image,
+            _effect_chain: Vec::new(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = Configuration {
+        input: Input::Path(None),
+        output: Output::Dump,
+    };
+
+    let mut args = env::args();
+
+    let _ = args.next(); // ignore first item
+
+    let mut input_path_arg: Option<String> = None;
+
+    while let Some(arg) = args.next() {
+        match arg.trim() {
+            "-i" => {
+                input_path_arg = Some(String::from(args.next().unwrap().trim()));
+            }
+            "-o" => {
+                config.output = Output::Path(String::from(args.next().unwrap().trim()));
+            }
+            "-pipe" => {
+                config.input = Input::Pipe;
+            }
+            "-dump" => {
+                config.output = Output::Dump;
+            }
+            _ => panic!("couldn't parse input"),
+        }
+    }
+
+    let input_path;
+    match input_path_arg {
+        None => (),
+        Some(path) => {
+            input_path = path;
+            config.input = Input::Path(Some(Path::new(&input_path)));
+        }
+    }
+
+    let imgbuf = match config.input {
+        Input::Pipe => {
+            let mut buf: Vec<u8> = Vec::new();
+            let stdin = std::io::stdin();
+            let mut handle = stdin.lock();
+
+            match handle.read_to_end(&mut buf) {
+                Result::Err(_x) => (),
+                Result::Ok(x) => {
+                    eprintln!("read {} bytes", x)
+                }
+            };
+
+            let buf = Cursor::new(buf);
+
+            image::ImageReader::new(buf)
+                .with_guessed_format()
+                .unwrap()
+                .decode()
+                .unwrap()
+       }
+        Input::Path(None) => panic!("input needed"),
+        Input::Path(Some(path)) => image::ImageReader::open(path).unwrap().decode().unwrap(),
+    };
+
+    let mut layers = Vec::new();
+    layers.push(Layer::new(imgbuf));
+
+    for _layer in layers.iter() {
+        eprintln!("hi");
+    }
+
+    let output_image = layers.first().unwrap().image.clone();
+
+    match config.output {
+        Output::Dump => {
+            let mut bytes: Vec<u8> = Vec::new();
+            output_image
+                .write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Png)
+                .unwrap();
+            io::stdout().write_all(bytes.as_slice()).unwrap()
+        }
+        Output::Path(path) => output_image.save(path).unwrap(),
+    }
 }
